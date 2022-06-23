c8b0890f07a088024bf6e90bf4aacb622f2b7855
Ryan Evaristo de Lima <ryan_evaristo@grupotiradentes.com>
2022-06-23 19:16:47 -0300

    terminando validações

--- secao-03/models.py
+++ secao-03/models.py
@@ -9,13 +9,26 @@ class Curso(BaseModel):
     horas: int
 
     @validator('titulo')
-    def validar_titulo(value):
+    def validar_titulo(value: str):
         palavras = value.split(' ')
         if len(palavras) < 3:
-            raise ValueError("O titulo deve ter mais do que 3 palavras")
+            raise ValueError("O titulo deve ter mais do que 2 palavras")
     
         return value
 
+    @validator('aulas')
+    def validar_aula(value: int): 
+        if value > 200:
+            raise ValueError("Esse curso ultrapassou o limite de horas")
+
+        return value
+
+    @validator('horas')
+    def validar_horas(value: int): 
+        if value > 1000:
+            raise ValueError("Esse curso ultrapassou o limite de horas")
+
+        return value
 
 cursos = [
     Curso(id=1, titulo="Programação para Leigos", aulas=20, horas=260),
